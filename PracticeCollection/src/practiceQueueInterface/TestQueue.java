fc18c1d21c3ba4e67f8d898c1fd805070c5530c7
Manali Toshniwal <itsmanalitoshniwal@gmail.com>
2023-04-28 15:24:16 +0530

    Deque as Queue and Stack

--- PracticeCollection/src/practiceQueueInterface/TestQueue.java
+++ PracticeCollection/src/practiceQueueInterface/TestQueue.java
@@ -17,6 +17,35 @@ public class TestQueue {
 		System.out.println("deleted element: "+q.poll());
 		System.out.println(q);
 		
+		Deque<Integer> dq= new LinkedList<>();
+		
+		//DeQue as Queue (FIFO)
+		dq.offerLast(10);
+		dq.offerLast(32);
+		dq.offerLast(12);
+		dq.offerLast(20);
+		dq.offerLast(10);
+		dq.offerLast(30);
+		
+		while(!dq.isEmpty()) {
+			Integer ele = dq.pollFirst();
+			System.out.println("Queue popped: "+ele);
+		}
+		
+		System.out.println();
+		
+		//Deque as Stack(LIFO)
+		dq.offerFirst(10);
+		dq.offerFirst(50);
+		dq.offerFirst(20);
+		dq.offerFirst(10);
+		dq.offerFirst(70);
+		dq.offerFirst(10);
+
+		while(!dq.isEmpty()) {
+			Integer ele = dq.pollLast();
+			System.out.println("Queue popped: "+ele);
+		}
 		
 	}
 
